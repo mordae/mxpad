7af5d1c770b318515e614590d2f8623f9de9145a
Jan Hamal Dvořák <mordae@anilinux.org>
2022-08-10 17:23:44 +0200

    Fix license
    Signed-off-by: Jan Hamal Dvořák <mordae@anilinux.org>

--- main/main.c
+++ main/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)  Jan Hamal Dvořák <mordae@anilinux.org>
+ * Copyright (C) 2022 Jan Hamal Dvořák <mordae@anilinux.org>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
